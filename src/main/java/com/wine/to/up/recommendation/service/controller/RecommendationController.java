edd2b82e559b74f1d971a405dc561555acd3b3cf
Roman Neviantsev <>
2020-09-29 20:35:05 +0300

    adding RecommendationController

--- src/main/java/com/wine/to/up/recommendation/service/controller/RecommendationController.java
+++ src/main/java/com/wine/to/up/recommendation/service/controller/RecommendationController.java
@@ -1,18 +1,18 @@
 package com.wine.to.up.recommendation.service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
+import com.wine.to.up.recommendation.service.api.recommendation.vo.RecommendationResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class RecommendationController {
 
-    @GetMapping(path="/recommendation/{userId:[\\d]+}")
-    public List<String> getByUserId(@PathVariable long userId) {
-        return Arrays.asList("thisOne", "andThatOne", String.format("for %d", userId));
+    @GetMapping(path = "/recommendation/{userId:[\\d]+}")
+    public RecommendationResponse getByRequest(@PathVariable long userId) {
+        return RecommendationResponse.builder().id(userId).build();
     }
 
 }
